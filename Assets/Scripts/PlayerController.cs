5b41d25b8099b7ce5b907ee03c3d45f515778406
raul sanchez <rsanchelop2@educacion.navarra.es>
2026-09-24 12:19:15 +0200

    Clase 24/09/2026 - Salto añadido

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigidBody2D;
+    Rigidbody2D rb;
     public float hVelocity = 10f;
-
+    public Collider2D feetCollider;
+    public LayerMask groundLayer;
+    public float jumpforce = 7f;
+    private bool isGrounded = false;
     int choques = 0;
     public float velocity = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -23,15 +26,22 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate() {
         float inputHorizontal = Input.GetAxis("Horizontal");
         //rigidBody2D.velocity = new Vector2(inputHorizontal * hVelocity, rigidBody2D.velocity.y);
-        rigidBody2D.AddForce(Vector2.right * inputHorizontal * hVelocity);
+        rb.AddForce(Vector2.right * inputHorizontal * hVelocity);
         
+        // verifica si el collider de los pies toca el suelo
+        isGrounded = feetCollider.IsTouchingLayers(groundLayer);
+        if (isGrounded && Input.GetKey(KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+        }
     }
 
-    void onCollisionEnter2D(Collision2D other){
+    void OnCollisionEnter2D(Collision2D other){
         Debug.Log("Colision con: " + other.gameObject.name);
         choques++;
         if (choques == 2){
             Debug.Log("Has ganado");
         }
+
+        
     }
 }
